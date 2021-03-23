436b1219277c99d14ef671193f82e0b2d3030196
AnDrew0o <57329824+AnDrew0o@users.noreply.github.com>
2021-03-24 00:07:12 +0200

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static int Factorial(int num)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+
+
+            return factorial;
+        }
+
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Factorial(num));
+        }
+    }
+}
