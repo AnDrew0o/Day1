f0e3392c4902a88b63b02b72ffead878aa04a7d7
AnDrew0o <57329824+AnDrew0o@users.noreply.github.com>
2021-03-23 21:39:43 +0200

    Add files via upload

---
+++ LeapYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static bool IsLeapYear(int year)
+        {
+            return ((year % 100 != 0 && year % 4 == 0) || (year % 100 == 0 && year % 400 == 0));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Ведiть рiк для перевiрки: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (IsLeapYear(year) == true)
+            {
+                Console.WriteLine(year + " рiк - високосний");
+            }
+            else
+            {
+                Console.WriteLine(year + " рiк - не високосний");
+            }
+        }
+    }
+}
